76dab7b3323694020d0a1b47571b62eb5ec6772c
dpatience627 <dpatience627@gmail.com>
2024-03-21 15:41:43 -0400

    Create HumanGuessesGameTest.java

---
+++ test/HumanGuessesGameTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class HumanGuessesGameTest {
+
+    @Test
+    void testMaxGuess() {
+        HumanGuessesGame guessGame = new HumanGuessesGame();
+        
+    }
+
+    @Test
+    void testMinGuess() {
+
+    }
+}
