8a0235bea6931a0f0088ff34ed9feaaee7c2aef3
jonathanghoma <91575899+jonathanghoma@users.noreply.github.com>
2024-03-21 11:24:28 -0400

    worked on some tests ;-;

--- test/ComputerGuessesPanelTest.java
+++ test/ComputerGuessesPanelTest.java
@@ -1,10 +1,12 @@
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ComputerGuessesPanelTest {
     @Test
     void testMaxGuess(){
-
+        ComputerGuessesPanel toTest = new ComputerGuessesPanel();
+        toTest.setInitial();
+        //assertEquals(1, toTest.setLowerBound());
 
     }
 }
