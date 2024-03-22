06b2a064780d12ef504e824555e794808cad1e07
dpatience627 <dpatience627@gmail.com>
2024-03-21 21:47:10 -0400

    tests

--- test/GameOverPanelTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-
-public class GameOverPanelTest {
-    @Test
-    void isThisTestDouble(){
-
-    }
-}
--- test/HumanGuessesGameTest.java
+++ test/HumanGuessesGameTest.java
@@ -21,6 +21,27 @@ public class HumanGuessesGameTest {
         assertEquals(4, guessGame.getNumGuesses());
     }
     @Test
+    void testManyGuesses() {
+        HumanGuessesGame guessGame = new HumanGuessesGame(101);
+        guessGame.makeGuess(700);
+        guessGame.makeGuess(350);
+        guessGame.makeGuess(50);
+        guessGame.makeGuess(300);
+        guessGame.makeGuess(200);
+        guessGame.makeGuess(150);
+        guessGame.makeGuess(60);
+        guessGame.makeGuess(68);
+        guessGame.makeGuess(76);
+        guessGame.makeGuess(999);
+        guessGame.makeGuess(88);
+        guessGame.makeGuess(91);
+        guessGame.makeGuess(95);
+        guessGame.makeGuess(98);
+        guessGame.makeGuess(100);
+        guessGame.makeGuess(101);
+        assertEquals(16, guessGame.getNumGuesses());
+    }
+    @Test
     void testIsDoneTrue() {
         HumanGuessesGame guessGame = new HumanGuessesGame(1000);
         guessGame.makeGuess(500);
